bfe91632ba82973ac1cd66c76d71ec0b379dc83a
Gustavo Luis <gustavoluis202@gmail.com>
2022-01-02 18:15:07 -0300

    Projeto completo!

---
+++ ContaBancaria/Entities/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using ContaBancaria.Entities.Exceptions;
+namespace ContaBancaria.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > Balance)
+            {
+                throw new DomainException("Erro no saque! Saldo insuficiente.");
+            }
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("Erro no saque! Limite de saque excedido.");
+            }
+
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Saldo atual: {Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using ContaBancaria.Entities;
+using ContaBancaria.Entities.Exceptions;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Entre com os dados da conta");
+
+                Console.Write("Número: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Titular: ");
+                string holder = Console.ReadLine();
+                Console.Write("Saldo: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Limite de Saque: ");
+                double whitdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, whitdrawLimit);
+
+                Console.Write("\nInsira o valor para sacar: ");
+                double whitdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(whitdraw);
+
+                Console.WriteLine($"\n{account}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro inesperado! {ex.Message}");
+            }
+        }
+    }
+}
